c18c700950e6c95da63a78343c643a23b5eb466d
bitLabs <75418136+bitlabs-job-ready@users.noreply.github.com>
2022-03-25 19:06:18 +0530

    Add files via upload

---
+++ PBFMT5_For_Loop.py
@@ -0,0 +1,6 @@
+"""
+For Loop
+
+Write a Python Program that you want to specify the range from 0 to 11, since you want to display only the numbers in the sequence up to 55, which is the 11th number in the sequence.
+
+"""
